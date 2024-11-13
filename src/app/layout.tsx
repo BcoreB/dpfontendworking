2cb803b80ce73d53242cd9194e520fc8202daa52
Ujwal Vinay <walkerdoodle7@gmail.com>
2024-11-13 14:26:58 +0530

    HR dashboard branch stable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,15 +46,13 @@ export default function RootLayout({
 }>) {
   return (
     <DirectionProvider>
-<<<<<<< HEAD
       <EmployeeProvider>
-        <div className={inter.className}>
-=======
+
         <div className='{inter.className}'>
->>>>>>> 0e71afe813de9cf64f5ddfbbe7141072d4f33def
           <HideableNavbar />
           {children}
         </div>
+      </EmployeeProvider>
     </DirectionProvider>
   );
 }
